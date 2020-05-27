efb64030e0ce2fb3c53ed10387b7fa831ab1ea9e
phillipug <phillipmusiime@gmail.com>
2020-05-27 12:19:58 +0300

    corrected rubocop errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-	validates :title, :body, presence: true
+  validates :title, :body, presence: true
 
   belongs_to :user
   has_many :comments
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-	validates :name, presence: true, length: {minimum: 2, maximum: 20}, uniqueness: true
+  validates :name, presence: true, length: { minimum: 2, maximum: 20 }, uniqueness: true
 
-	has_many :posts
-	has_many :comments
+  has_many :posts
+  has_many :comments
 end
